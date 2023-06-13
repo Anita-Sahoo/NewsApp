bc97f6c9c22c085b1064ca2fd154e60a193909b7
Anita Sahoo <sahooanita066@gmail.com>
2023-06-13 22:21:53 +0530

    This is my first commit using cmd

---
+++ src/sample.js
@@ -0,0 +1,84 @@
+{
+  "status" = "ok",
+  "totalResults"= 6,
+  "articles"= [
+    {
+      "source": {
+        "id": "independent",
+        "name": "Independent"
+      },
+      "author": "Jack Rathborn",
+      "title": "England cricket team bus blocked by Just Stop Oil protestors",
+      "description": "The England team bus was delayed by five minutes on its way from the team hotel to Lord’s for day one of the Test against Ireland",
+      "url": "http://www.independent.co.uk/sport/cricket/england-cricket-bus-just-stop-oil-ireland-b2349479.html",
+      "urlToImage": "https://static.independent.co.uk/2023/06/01/09/FxhbD--WcAA1FcI.jpg?quality=75&width=1200&auto=webp",
+      "publishedAt": "2023-06-01T08:25:12Z",
+      "content": "Sign up to our free sport newsletter for all the latest news on everything from cycling to boxing\r\nSign up to our free sport email for all the latest news\r\nEnglands bus was blocked on its way to Lord… [+556 chars]"
+    },
+    {
+      "source": {
+        "id": "news-com-au",
+        "name": "News.com.au"
+      },
+      "author": "Liam Beatty",
+      "title": "Cricket exec abused sleeping colleagues",
+      "description": "<p>A former senior executive at Cricket Australia has been found guilty of taking advantage of two young sleeping colleagues. </p>",
+      "url": "https://www.news.com.au/sport/cricket/timothy-whittaker-former-cricket-australia-executive-found-guilty-of-sexually-assaulting-two-sleeping-colleagues/news-story/4250cd1d57895d7849a2ef1757c21aa4",
+      "urlToImage": "https://content.api.news/v3/images/bin/8873ca52aa1b75edd9c2d37153b58384",
+      "publishedAt": "2023-06-01T07:39:00Z",
+      "content": "A former senior executive at Cricket Australia has been found guilty of taking advantage of two young sleeping colleagues. \r\nTimothy Joseph Whittaker, 38, returned before the Melbourne Magistrates’ C… [+3299 chars]"
+    },
+    {
+      "source": {
+        "id": "google-news-au",
+        "name": "Google News (Australia)"
+      },
+      "author": "The Age",
+      "title": "Former Cricket Australia executive guilty of sexually assaulting two men - The Age",
+      "description": null,
+      "url": "https://news.google.com/rss/articles/CBMiiQFodHRwczovL3d3dy50aGVhZ2UuY29tLmF1L25hdGlvbmFsL3ZpY3RvcmlhL2Zvcm1lci1jcmlja2V0LWF1c3RyYWxpYS1leGVjdXRpdmUtZ3VpbHR5LW9mLXNleHVhbGx5LWFzc2F1bHRpbmctdHdvLW1lbi0yMDIzMDUzMS1wNWRjcXMuaHRtbNIBiQFodHRwczovL2FtcC50aGVhZ2UuY29tLmF1L25hdGlvbmFsL3ZpY3RvcmlhL2Zvcm1lci1jcmlja2V0LWF1c3RyYWxpYS1leGVjdXRpdmUtZ3VpbHR5LW9mLXNleHVhbGx5LWFzc2F1bHRpbmctdHdvLW1lbi0yMDIzMDUzMS1wNWRjcXMuaHRtbA?oc=5",
+      "urlToImage": null,
+      "publishedAt": "2023-06-01T06:17:45+00:00",
+      "content": null
+    },
+    {
+      "source": {
+        "id": "bbc-sport",
+        "name": "BBC Sport"
+      },
+      "author": null,
+      "title": "'Ashes summer can breathe life into Test cricket'",
+      "description": "England's match against Ireland kicks off a hugely important summer for Test cricket and an incredibly exciting six-month period, says Jonathan Agnew.",
+      "url": "http://www.bbc.co.uk/sport/cricket/65770035",
+      "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/144C8/production/_129944138_gettyimages-1494596580.jpg",
+      "publishedAt": "2023-06-01T05:22:16.7465236Z",
+      "content": "England have won 10 of their 12 Tests since Brendon McCullum (left) became head coach with Ben Stokes (right) as captain\r\n<table><tr><th>England v Ireland</th></tr>\r\n<tr><td>Venue: Lord's Dates: 1-4 … [+6066 chars]"
+    },
+    {
+      "source": {
+        "id": "espn-cric-info",
+        "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+      "publishedAt": "2020-04-27T11:41:47Z",
+      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    },
+    {
+      "source": {
+        "id": "espn-cric-info",
+        "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      "publishedAt": "2020-03-30T15:26:05Z",
+      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    }
+  ]
+}
